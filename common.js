344273cb4ba7e5dd318f910b52fb64a70bd5d9a2
Egorjs <mail_to_egr@mail.ru>
2017-11-28 22:28:38 +0300

    Sort 1

--- common.js
+++ common.js
@@ -1,6 +1,12 @@
 var age = document.getElementsByClassName('age');
 
-
 for (var i = 0; i < age.length; i++) {
-    console.log(age[i]);
+    for (var j = 0; j < age.length - 1; j++) {
+        if (age[j].innerHTML > age[j + 1].innerHTML) {
+            var k = age[j].innerHTML;
+            age[j].innerHTML = age[j + 1].innerHTML;
+            age[j + 1].innerHTML = k;
+        }
+
+    }
 }
